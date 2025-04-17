e6702d4f6bb1b0e9a8cade1a96337d4ed7d6db59
parth2024 <156824891+parth2024@users.noreply.github.com>
2025-04-17 09:58:39 +0530

    Added env vars

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 # from .tasks import test_task
 # from decouple import config
 # Create your views here.
@@ -7,4 +8,7 @@ from django.http import HttpResponse
 def index(request):
     # test_task.delay()
 
-    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123 - harsh43')
+    # Accessing an environment variable
+    debug_mode = os.environ.get('DEBUG', 'Not Set')
+
+    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123 - harsh43 {debug_mode}\n')
