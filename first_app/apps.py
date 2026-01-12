3f3907e803f4000d8609aeb594d0b3b2a651b3cf
parth2024 <156824891+parth2024@users.noreply.github.com>
2026-01-12 16:16:34 +0530

    Update apps.py

--- first_app/apps.py
+++ first_app/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class FirstAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'   
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'first_app' 
 #ok
 #cdcdc
